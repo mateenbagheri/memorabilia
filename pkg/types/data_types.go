47871e1b6b2a0ef608e8d6fd703bd562d735a31d
mateen bagheri <mateenbagheri@mateens-MacBook-Pro.local>
2024-08-09 13:35:08 +0330

    add memorabilia data type system and its tests

--- pkg/types/data_types.go
+++ pkg/types/data_types.go
@@ -6,55 +6,82 @@ import (
 	"strconv"
 )
 
-var ErrNoneCastable = errors.New("can not cast error")
+// ErrNoneCastable is an error indicating that a value cannot be cast to the desired type.
+var ErrNoneCastable = errors.New("cannot cast error")
 
+// ColumnType is an enum type for different column data types.
 type ColumnType int
 
 const (
+	// IntType represents integer data type.
 	IntType ColumnType = iota
+	// StringType represents string data type.
 	StringType
+	// FloatType represents floating-point data type.
 	FloatType
 )
 
+// ColumnValue is an interface that defines methods for working with column values.
 type ColumnValue interface {
+	// Value returns the underlying value of the column.
 	Value() any
+	// Type returns the type of the column as a ColumnType.
 	Type() ColumnType
-	ToInt() int
+	// ToInt converts the column value to an integer, if possible.
+	ToInt() (int, error)
+	// ToString converts the column value to a string.
 	ToString() string
+	// ToFloat converts the column value to a float64
+	ToFloat() (float64, error)
 }
 
+// Integer represents a column value of integer type.
 type Integer struct {
 	Val int
 }
 
+// Value returns the integer value.
 func (v Integer) Value() any {
 	return v.Val
 }
 
-func (v Integer) ToInt() int {
-	return v.Val
+// ToInt returns the integer value directly.
+func (v Integer) ToInt() (int, error) {
+	return v.Val, nil
 }
 
+// ToString converts the integer value to a string.
 func (v Integer) ToString() string {
 	return fmt.Sprint(v.Val)
 }
 
+// ToFloat converts an integer to a float
+func (v Integer) ToFloat() (float64, error) {
+	return float64(v.Val), nil
+}
+
+// Type returns the ColumnType for integer, which is IntType.
 func (v Integer) Type() ColumnType {
 	return IntType
 }
 
+// String represents a column value of string type.
 type String struct {
 	Val string
 }
 
+// Value returns the string value.
 func (v String) Value() any {
 	return v.Val
 }
 
+// ToString returns the string value directly.
 func (v String) ToString() string {
 	return v.Val
 }
 
+// ToInt attempts to convert the string value to an integer.
+// Returns the integer and nil if successful, otherwise returns 0 and ErrNoneCastable.
 func (v String) ToInt() (int, error) {
 	i, err := strconv.Atoi(v.Val)
 	if err != nil {
@@ -63,6 +90,62 @@ func (v String) ToInt() (int, error) {
 	return i, nil
 }
 
+// ToFloat attempts to convert the string value to a float64.
+// Returns the float64 and nil if successful, otherwise returns 0.0 and ErrNonCastable.
+func (v String) ToFloat() (float64, error) {
+	f, err := strconv.ParseFloat(v.Val, 64)
+	if err != nil {
+		return 0.0, ErrNoneCastable
+	}
+	return f, nil
+}
+
+// Type returns the ColumnType for string, which is StringType.
 func (v String) Type() ColumnType {
 	return StringType
+}
+
+// Float represents a column value of float type.
+type Float struct {
+	Val float64
+}
+
+// Value returns the float value.
+func (v Float) Value() any {
+	return v.Val
+}
+
+// ToInt converts the float value to an integer.
+func (v Float) ToInt() (int, error) {
+	return int(v.Val), nil
+}
+
+// ToString converts the float value to a string.
+func (v Float) ToString() string {
+	return fmt.Sprint(v.Val)
+}
+
+func (v Float) ToFloat() (float64, error) {
+	return v.Val, nil
+}
+
+// Type returns the ColumnType for float, which is FloatType.
+func (v Float) Type() ColumnType {
+	return FloatType
+}
+
+// DetectColumnType takes a string input and determines its appropriate ColumnType.
+func DetectColumnType(input string) (ColumnType, ColumnValue) {
+	// Attempt to parse as an integer
+	if i, err := strconv.Atoi(input); err == nil {
+		return IntType, Integer{Val: i}
+	}
+
+	// Attempt to parse as a float
+	if f, err := strconv.ParseFloat(input, 64); err == nil {
+		return FloatType, Float{Val: f}
+	}
+
+	// If neither, it's considered a string type
+	return StringType, String{Val: input}
 }
--- pkg/types/data_types_test.go
+++ pkg/types/data_types_test.go
@@ -6,7 +6,10 @@ import (
 
 func TestInteger_ToInt(t *testing.T) {
 	i := Integer{Val: 42}
-	if got := i.ToInt(); got != 42 {
+	if got, err := i.ToInt(); err != nil || got != 42 {
+		if err != nil {
+			t.Errorf("Expected no errors but got following error = %v", err)
+		}
 		t.Errorf("ToInt() = %v, want %v", got, 42)
 	}
 }
@@ -15,6 +18,18 @@ func TestInteger_ToString(t *testing.T) {
 	i := Integer{Val: 42}
 	if got := i.ToString(); got != "42" {
 		t.Errorf("ToString() = %v, want %v", got, "42")
+	}
+}
+
+func TestInteger_ToFloat(t *testing.T) {
+	f := Float{Val: 40.24}
+	got, err := f.ToFloat()
+	if err != nil {
+		t.Errorf("ToFloat() expected no errors, got %v", err)
+	}
+
+	if got != 40.24 {
+		t.Errorf("ToFloat() = %v, want %v", got, 42.2)
 	}
 }
 
@@ -50,6 +65,33 @@ func TestString_ToInt(t *testing.T) {
 	}
 }
 
+func TestString_ToFloat(t *testing.T) {
+	tests := []struct {
+		name    string
+		s       String
+		want    float64
+		wantErr bool
+	}{
+		{"valid integer value", String{Val: "42"}, 42.0, false},
+		{"valid floating value", String{Val: "23.2"}, 23.2, false},
+		{"valid zero value", String{Val: "0"}, 0.0, false},
+		{"invalid value", String{Val: "test"}, 0.0, true},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := tt.s.ToFloat()
+			if err != nil && !tt.wantErr {
+				t.Errorf("ToFloat() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("ToFloat() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
 func TestString_Type(t *testing.T) {
 	s := String{Val: "test"}
 	if got := s.Type(); got != StringType {
@@ -64,6 +106,13 @@ func TestInteger_Type(t *testing.T) {
 	}
 }
 
+func TestFloat_Type(t *testing.T) {
+	f := Float{Val: 12.5}
+	if got := f.Type(); got != FloatType {
+		t.Errorf("Type() = %v, want %v", got, FloatType)
+	}
+}
+
 func TestInteger_Value(t *testing.T) {
 	i := Integer{Val: 42}
 	if got := i.Value(); got != 42 {
@@ -75,5 +124,40 @@ func TestString_Value(t *testing.T) {
 	s := String{Val: "hello"}
 	if got := s.Value(); got != "hello" {
 		t.Errorf("Value() = %v, want %v", got, "hello")
+	}
+}
+
+func TestFloat_Value(t *testing.T) {
+	f := Float{Val: 23.6}
+	if got := f.Value(); got != 23.6 {
+		t.Errorf("Value() = %v, want %v", got, 23.6)
+	}
+}
+
+func TestDetectColumnType(t *testing.T) {
+	tests := []struct {
+		name          string
+		input         string
+		expectedType  ColumnType
+		expectedValue ColumnValue
+	}{
+		{"Integer Value", "123", IntType, Integer{Val: 123}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			columnType, value := DetectColumnType(tt.input)
+			if columnType != tt.expectedType {
+				t.Errorf("DetectColumnType(%v) = %v, want %v",
+					tt.input, columnType, tt.expectedType,
+				)
+			}
+
+			if value != tt.expectedValue {
+				t.Errorf("DetectColumnType(%v)::Value = %v, want %v",
+					tt.input, value, tt.expectedValue,
+				)
+			}
+		})
 	}
 }
